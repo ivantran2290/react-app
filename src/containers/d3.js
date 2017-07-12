a117d75cb2b0c4b001789e2ec093095253c90cd0
Ivan <ivantran2290@gmail.com>
2017-07-13 00:57:04 +0900

    #add dragging

--- src/containers/d3.js
+++ src/containers/d3.js
@@ -70,17 +70,64 @@ class D3App extends React.Component {
       .attr("y1", pointC.y)
       .attr("x2", pointD.x)
       .attr("y2", pointD.y)
-      .attr("stroke", "#F06292");
-
-    this.drawPoint(svg, new Point(30, 30))
+      .attr("stroke", "#F06292");       
   }
 
   drawPoint(svg, coordinates){
+    var drag = d3.drag()
+      .on("drag", function(d){
+        var x = d3.event.x;
+        var y = d3.event.y;
+        d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+      })
+      .on("start", function(d){
+        d3.select(this).raise().classed("active", true);
+      })
+      .on("end", function(d){
+        d3.select(this).classed("active", false);
+      });
+    
+    var randomColor = (function(){
+    var golden_ratio_conjugate = 0.618033988749895;
+    var h = Math.random();
+
+    var hslToRgb = function (h, s, l){
+        var r, g, b;
+
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return '#'+Math.round(r * 255).toString(16)+Math.round(g * 255).toString(16)+Math.round(b * 255).toString(16);
+    };
+    
+    return function(){
+      h += golden_ratio_conjugate;
+      h %= 1;
+      return hslToRgb(h, 0.5, 0.60);
+    };
+  })();
+
     svg.append("circle")
-      .attr("cx",coordinates.x)
-      .attr("cy",coordinates.y)
-      .attr("r", 5)
-      .attr("fill", "#FF5722");
+      .attr("transform", "translate(" + coordinates.x + "," + coordinates.y + ")")
+      .attr("r", 20)
+      .attr("fill", randomColor)
+      .call(drag);    
   }
 
   componentDidMount() {
