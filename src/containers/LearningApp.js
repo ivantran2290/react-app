d89c08a332560f4f208c723a6cc75d174d69fa5b
Ivan <ivantran2290@gmail.com>
2017-08-07 23:34:36 +0900

    #add thu6 ngay3 app

--- src/containers/LearningApp.js
+++ src/containers/LearningApp.js
@@ -4,6 +4,8 @@ import CommonUtils from '../utils/commonUtils';
 class LearningApp extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {text: new Date().getFullYear()};
   }
 
   getAllIntergerInArray(mangSoNguyen){
@@ -32,11 +34,37 @@ class LearningApp extends React.Component {
     }
   }
 
+  timThu6Ngay13TrongNam(nam){
+    //Khai bao bien ngay kq
+    let kq = [];
+    //Duyet tu vong th lap tu 1 den 12 va dieu kien kq == ngay 1/1/1
+    for (var index = 0; index < 12; index++) {
+      //Ngay = ngay 13 thang th nam nm
+      let date = new Date(nam, index, 13);
+
+      //Neu ngay la thu 6 -> gan kq = ngay vua tim thay -> xuat ngay ngay ra man hinh
+      if(date.getDay() == 5){
+        kq.push(CommonUtils.jpDateFormat(nam, index, 13));
+      }
+    }  
+    return kq;  
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value});    
+  }
+
   render() {
+    var kq = this.timThu6Ngay13TrongNam(this.state.text);
+    var list = [];
+    for (var i = 0; i < kq.length; i++) {
+      list.push(<div>{kq[i]}</div>);
+    }
     return (
       <div>
-        <h3>Programming Learning</h3>
-        <div>{this.getAllIntergerInArray([1,0,-1,2,5,6,-4])}</div>
+        <h3>Nhập vào một năm, tìm tất cả các ngày thứ 6 ngày 13 trong năm đó:</h3>
+        <input onChange={this.handleChange} value={this.state.text} />
+        <div>{list}</div>
       </div>
     );
   }
--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -1,60 +1,83 @@
 var CommonUtils = {
     randomColor: function(){
-    //var golden_ratio_conjugate = 0.618033988749895;
-    var h = Math.random();
+        //var golden_ratio_conjugate = 0.618033988749895;
+        var h = Math.random();
 
-    var hslToRgb = function (h, s, l){
-        var r, g, b;
+        var hslToRgb = function (h, s, l){
+            var r, g, b;
 
-        if(s === 0){
-            r = g = b = l; // achromatic
-        }else{
-            function hue2rgb(p, q, t){
-                if(t < 0) t += 1;
-                if(t > 1) t -= 1;
-                if(t < 1/6) return p + (q - p) * 6 * t;
-                if(t < 1/2) return q;
-                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-                return p;
+            if(s === 0){
+                r = g = b = l; // achromatic
+            }else{
+                function hue2rgb(p, q, t){
+                    if(t < 0) t += 1;
+                    if(t > 1) t -= 1;
+                    if(t < 1/6) return p + (q - p) * 6 * t;
+                    if(t < 1/2) return q;
+                    if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                    return p;
+                }
+
+                var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+                var p = 2 * l - q;
+                r = hue2rgb(p, q, h + 1/3);
+                g = hue2rgb(p, q, h);
+                b = hue2rgb(p, q, h - 1/3);
             }
 
-            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            var p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1/3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1/3);
-        }
+            return '#'+Math.round(r * 255).toString(16)+Math.round(g * 255).toString(16)+Math.round(b * 255).toString(16);
+        };
 
-        return '#'+Math.round(r * 255).toString(16)+Math.round(g * 255).toString(16)+Math.round(b * 255).toString(16);
-    };
-
-    return hslToRgb(h, 0.5, 0.60);
+        return hslToRgb(h, 0.5, 0.60);
     },
     calDistance2Point: function(startP, endP){
-    return Math.sqrt(Math.pow((endP.x - startP.x),2) + Math.pow((endP.y - startP.y),2))
+        return Math.sqrt(Math.pow((endP.x - startP.x),2) + Math.pow((endP.y - startP.y),2))
     },
     convertPIToDegrees: function(angle){
         return (angle > 0 ? angle : (2*Math.PI + angle)) * 360 / (2*Math.PI)
     },
     giaiPhuongTrinhBacHai: function(a, b, c){
-    let nghiem = [];
+        let nghiem = [];
 
-    let delta = Math.pow(b, 2) - 4 * a * c;
-    if (delta > 0)
-    {
-        nghiem[0] = (-b - Math.sqrt(delta)) / 2 * a;
-        nghiem[1] = (-b + Math.sqrt(delta)) / 2 * a;
+        let delta = Math.pow(b, 2) - 4 * a * c;
+        if (delta > 0)
+        {
+            nghiem[0] = (-b - Math.sqrt(delta)) / 2 * a;
+            nghiem[1] = (-b + Math.sqrt(delta)) / 2 * a;
+        }
+        else if (delta == 0)
+        {
+            nghiem[0] = -b / 2 * a;
+        }
+        else if (delta < 0)
+        {
+            nghiem = [];
+        }    
+        return nghiem;    
+    },
+    jpDateFormat: function(year, month, day){
+        let dayOfWeek = this.jpDayOfWeek(year, month, day);
+        return year + "年" + (month+1) + "月" + day + "日（" + dayOfWeek + "）";
+    },
+    jpDayOfWeek: function(year, month, day){
+    let date = new Date(year, month, day);
+    switch(date.getDay()){
+      case 0:
+        return "日";
+      case 1:
+        return "月";
+      case 2:
+        return "火";
+      case 3:
+        return "水";
+      case 4:
+        return "木";
+      case 5:
+        return "金";
+      case 6:
+        return "土";
     }
-    else if (delta == 0)
-    {
-        nghiem[0] = -b / 2 * a;
-    }
-    else if (delta < 0)
-    {
-        nghiem = [];
-    }    
-    return nghiem;    
-    }
+  }
 };
 
 export default CommonUtils;
